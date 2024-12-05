f6001557545f92fdeec20eb50505f06ba6a12a52
royak47 <52905147+royak47@users.noreply.github.com>
2024-12-05 08:32:43 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -7,15 +7,18 @@ app.use(bodyParser.json());
 
 let userData = {}; // Temporary in-memory storage
 
+// Endpoint to save user data
 app.post('/saveData', (req, res) => {
     const { userId, points } = req.body;
     userData[userId] = points;
     res.send({ status: 'success' });
 });
 
+// Endpoint to retrieve user data
 app.get('/getData/:userId', (req, res) => {
     const userId = req.params.userId;
     res.send({ points: userData[userId] || 0 });
 });
 
+// Start the server
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
